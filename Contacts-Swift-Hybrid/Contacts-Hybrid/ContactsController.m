4d924b969e367492c88d08c750261ffca6da0daf
brys <brys1saclaus2@gmail.com>
2020-12-01 19:04:02 -0800

    //

--- Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.m
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.m
@@ -7,26 +7,22 @@
 //
 
 #import "ContactsController.h"
-
+#import "Contacts_Hybrid-Swift.h"
 @interface ContactsController ()
-
 @property (nonatomic) NSMutableArray *internalContacts;
-
 @end
-
 @implementation ContactsController
-
 - (instancetype)init
 {
     if (self = [super init]) {
         _internalContacts = [[NSMutableArray alloc] init];
+        Contact *contact = [[Contact alloc] initWithName:@"Bryson" relationship:@"Myself"];
+        [_internalContacts addObject:contact];
     }
     return self;
 }
-
 - (NSArray *)contacts
 {
     return self.internalContacts.copy;
 }
-
 @end
